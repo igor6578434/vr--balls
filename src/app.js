de5c3f7063546af8d3ff5406a212a22ac8fb60c5
igor <igorkarapysh3@gmail.com>
2021-12-26 19:10:46 +0200

    Refactor

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import * as THREE from 'three/build/three.module.js'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import {VRButton} from "three/examples/jsm/webxr/VRButton"
 import {BoxLineGeometry} from "three/examples/jsm/geometries/BoxLineGeometry"
-import {compress} from "three/examples/jsm/libs/fflate.module";
+import {XRControllerModelFactory} from "three/examples/jsm/webxr/XRControllerModelFactory";
 
 class App {
   constructor() {
@@ -37,6 +37,8 @@ class App {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
     this.controls.target.set(0, 1.6, 0)
     this.controls.update()
+
+    this.controllers = []
 
     // this.initSceneCube()
     this.initScene()
@@ -88,14 +90,122 @@ this.radius = 0.09
        object.position.z = this.random(-2, 2 )
 
       this.room.add(object)
+    }
 
+    this.highlight =new THREE.Mesh ( geometry, new THREE.MeshBasicMaterial( {
+      color: 0xFFFFFF, side: THREE.BackSide }))
+    this.highlight.scale.set(1.2, 1.2, 1.2)
+    this.scene.add( this.highlight)
+  }
+
+  setupVR() {
+    this.renderer.xr.enabled = true
+    document.body.appendChild(VRButton.createButton(this.renderer))
+
+    let i = 0
+    // this.flashLightController(i++)
+    this.buildStandardController(i++)
+    //this.flashLightController(i++)
+    this.buildStandardController(i++)
+  }
+
+  flashLightController(index) {
+    const self = this
+
+    function onSelectStart() {
+      this.userData.selectPressed = true
+      if (self.sportlights[this.uuid]) {
+        self.sportlights[this.uuid].visible = true
+      } else {
+      }
+      this.children [0].scale.z = 10
     }
   }
 
-  setupVR(){
-this.renderer.xr.enabled = true
-    document.body.appendChild(VRButton.createButton(this.renderer))
+  buildStandardController(index) {
+    const controllerModelFactory = new XRControllerModelFactory()
+    const geometry = new THREE.BufferGeometry().setFromPoints([
+      new THREE.Vector3(0, 0, 0),
+      new THREE.Vector3(0, 0, -1),
+    ])
+    const line = new THREE.Line(geometry)
+    line.name = 'line'
+    line.scale.z = 0
+
+    const controller = this.renderer.xr.getController(index)
+
+    controller.add(line)
+    controller.userData.selectPressed = false
+
+    const grip = this.renderer.xr.getControllerGrip(index)
+    grip.add(controllerModelFactory.createControllerModel(grip))
+    this.scene.add(grip)
+
+    const self = this
+
+    function onSelectStart() {
+      this.children[0].scale.z = 0
+      self.highlight.visible = false
+      this.userData.selectPressed = false
+    }
+
+    function onSelectEnd() {
+      this.children[0].scale.z = 0
+      self.highlight.visible = false
+      this.userData.selectPressed = false
+    }
+
+    controller.addEventListener('selectstart', onSelectStart);
+    controller.addEventListener('selectend', onSelectEnd);
+
+    controller.handle = () => this.handleController(controller)
+
+    this.scene.add(controller)
+    this.controllers[index] = controller
   }
+
+  handleController(controller){
+    if (controller.userData.selectPressed) {
+      controller.children[0].scale.z = 10
+      this.workingMatrix.identity().extractRotation( controller.matrixWorld)
+
+      this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld)
+
+      this.raycaster.ray.direction.set(0, 0, -1).applyMatrix4(this.workingMatrix)
+
+      const intersects = this.raycaster.intersect0bjects(this.room.children)
+      if (intersects.length > 0) {
+        if (intersects[0].object.uuid !== this.highlight.uuid) {
+          intersects [0].object.add(this.highlight)
+        }
+        this.highlight.visible = true
+        controller.children[0].scale.z = intersects[0].distance
+      } else {
+        this.highlight.visible = false
+      }
+    }
+  }
+
+  handleFlashLightController (controller) {
+    if (controller.userData.selectPressed) {
+      this.workingMatrix.identity().extractRotation( controller.matrixWorld)
+
+      this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld)
+
+      this.raycaster.ray.direction.set(0, 0, -1).applyMatrix4(this.workingMatrix)
+
+      const intersects = this.raycaster.intersect0bjects(this.room.children)
+
+      if (intersects.length> 0) {
+        if (intersects[0].object.uuid !== this.highlight) {
+          intersects[0].object.add(this.highlight)
+        }
+        this.highlight.visible = true
+      }else {
+        this.highlight.visible = false
+       }
+      }
+    }
 
   resize() {
     this.camera.aspect = window.innerWidth / window.innerHeight
